057e967357b4305d5830087094b3373dabc8e023
Jason Rylance <ecnalyr@gmail.com>
2013-02-25 08:12:55 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikeshop::Application.config.secret_token = '8dc8e8481e6f9724005154ffe9508d1ed64ff050c124a0d50b4dbb72cc11c1553c99cd49e1dc6e40bc0309517f6bec3ae07b6845d9dafe5092a3d2f11d06ba9e'
