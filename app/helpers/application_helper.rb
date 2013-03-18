5672b6f488a73369b163524b18605aa04a22f2be
Jason Rylance <ecnalyr@gmail.com>
2013-03-18 09:24:26 -0500

    Upated date line in header to dynamically update and include hours other than today's

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  require 'date'
+  require 'time'
+  def todays_hours
+    now = Time.now
+    html = ""
+    if now.wday == 0
+      html << "Closed Sundays"
+    elsif now.wday == 6
+      html << "Open today until 5pm"
+    else
+      html << "Open today until 7pm"
+    end
+    html
+  end
 end
